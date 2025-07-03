bf77e22a72511c6acafb732c5adf9e718dacef21
Hilbert Kong <kongliangxm@foxmail.com>
2025-07-03 19:28:28 +0900

    Update test script

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ import sys
 sys.path.append('core')
 
 import argparse
-from PIL import Image
 import torch
-import torchvision.transforms as tf
+import cv2
 
 from utils.flow_viz import flow_to_image
+from utils.frame_utils import writeFlow
 from utils.utils import InputPadder
 from flowdiffuser import FlowDiffuser
 
@@ -17,9 +17,8 @@ elif torch.cuda.is_available():
 else:
     device = torch.device("cpu")
 
-transform = tf.Compose([
-    tf.PILToTensor()
-])
+def to_tensor(img, device=device):
+    return torch.from_numpy(img.transpose(2, 0, 1)).unsqueeze(0).float().to(device)
 
 
 parser = argparse.ArgumentParser()
@@ -29,27 +28,24 @@ parser.add_argument('--small', action='store_true', help='use small model')
 parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
 parser.add_argument('--alternate_corr', action='store_true', help='use efficent correlation implementation')
 args = parser.parse_args()
-model = FlowDiffuser(args)
-model.load_state_dict(torch.load('weights/FlowDiffuser-things.pth'), strict=False)
+model = torch.nn.DataParallel(FlowDiffuser(args))
+model.load_state_dict(torch.load('weights/FlowDiffuser-things.pth'))
 model.to(device)
 model.eval()
 
 with torch.no_grad():
-    img0 = Image.open('sketch1.png').convert('RGB')
-    img1 = Image.open('sketch3.png').convert('RGB')
-    img0 = transform(img0)
-    img1 = transform(img1)
-    img0 = img0.to(device)
-    img1 = img1.to(device)
-    img0 = img0.unsqueeze(0)
-    img1 = img1.unsqueeze(0)
+    img0 = cv2.imread('frame1.png')
+    img1 = cv2.imread('frame2.png')
+    img0 = to_tensor(img0)
+    img1 = to_tensor(img1)
 
     padder = InputPadder(img0.shape)
     img0, img1 = padder.pad(img0, img1)
 
     flow_low, flow_pr = model(img0, img1, iters=32, test_mode=True)
-    flow = padder.unpad(flow_pr[0]).cpu()
-    flow_img = flow_to_image(flow)
-    flow_img.save('test.png')
-
+    flow = flow_pr.squeeze(0)
+    flow = padder.unpad(flow)
+    flow = flow.permute(1, 2, 0).cpu().numpy()
+    rgb = flow_to_image(flow)
+    cv2.imwrite('test.png', rgb)
 
